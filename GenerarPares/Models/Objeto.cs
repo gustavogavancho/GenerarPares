f11709192aeebadbdb55ff2e157f49e2dbb7bf3b
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2022-03-10 20:46:32 -0500

    Add project files.

---
+++ GenerarPares/Models/Objeto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenerarPares.Models;
+
+public class Objeto
+{
+    [Range(50, 500, ErrorMessage = "El número ingresado debe estar entre 50 y 500")]
+    public int Numero { get; set; }
+    public string Resultado { get; set; }
+}
---
+++ GenerarPares/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using GenerarPares.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace GenerarPares.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Objeto Objeto { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnPost()
+        {
+            for (int i = 0; i <= Objeto.Numero; i++)
+            {
+                if (i % 2 == 0)
+                    Objeto.Resultado += i.ToString() + " ";
+            }
+
+            return Page();
+        }
+    }
+}
